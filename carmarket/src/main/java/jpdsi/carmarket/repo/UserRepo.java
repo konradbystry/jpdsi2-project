8b5e51d864231fdfc5ac15008d848f60732b157b
Konrad Bystrzykowski <konor2000@gmail.com>
2021-11-12 12:51:12 +0100

    added query functions for UserService

--- carmarket/src/main/java/jpdsi/carmarket/repo/UserRepo.java
+++ carmarket/src/main/java/jpdsi/carmarket/repo/UserRepo.java
@@ -3,5 +3,9 @@ package jpdsi.carmarket.repo;
 import jpdsi.carmarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    void deleteUserById(Long id);
+    Optional<User> findUserById(Long id);
 }
