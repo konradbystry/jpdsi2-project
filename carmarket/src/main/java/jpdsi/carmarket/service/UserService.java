97097541b694923446e1ac9ddcf1fe4acf8e859c
Konrad Bystrzykowski <konor2000@gmail.com>
2021-11-12 12:52:33 +0100

    created User actions functions

--- carmarket/src/main/java/jpdsi/carmarket/service/UserService.java
+++ carmarket/src/main/java/jpdsi/carmarket/service/UserService.java
@@ -1,11 +1,29 @@
 package jpdsi.carmarket.service;
 
+import jpdsi.carmarket.exception.UserNotFoundException;
+import jpdsi.carmarket.model.User;
 import jpdsi.carmarket.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private final UserRepo userRepo;
 
+    @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    public User addUser(User user) {
+        return userRepo.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepo.deleteUserById(id);
+    }
+
+    public User findUserById(Long id) {
+        return (User) userRepo.findUserById(id).orElseThrow(()-> new UserNotFoundException("User with id " + id + " was not found"));
     }
 }
