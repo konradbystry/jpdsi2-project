b92f3492ee088ff6d479f2a5cbd13014c99ee8a0
konradbystry <konor2000@gmail.com>
2021-12-07 13:06:56 +0100

    created add car function

--- carmarket/src/main/java/jpdsi/carmarket/service/CarService.java
+++ carmarket/src/main/java/jpdsi/carmarket/service/CarService.java
@@ -1,2 +1,22 @@
-package jpdsi.carmarket.service;public class CarService {
+package jpdsi.carmarket.service;
+
+import jpdsi.carmarket.model.Car;
+import jpdsi.carmarket.repo.CarRepo;
+import jpdsi.carmarket.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+    public final CarRepo carRepo;
+
+    @Autowired
+    public CarService(CarRepo carRepo) {
+        this.carRepo = carRepo;
+    }
+
+    public Car addCar(Car car){
+        return carRepo.save(car);
+    }
+
 }
